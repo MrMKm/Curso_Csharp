2086d98dc4482f57b4b9f33449aca8e9f2657a71
Ricardo Orozco <mrmkm@live.com>
2022-04-05 11:54:02 -0500

    Fixing Predicates projects

---
+++ Predicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Predicates
+{
+    class Program
+    {
+        static void Predicates()
+        {
+            Predicate<int> predicate = new Predicate<int>(GetPrimos);
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> result = numbers.FindAll(predicate);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            static bool GetPairs(int num)
+            {
+                return num % 2 == 0 ? true : false;
+            }
+
+            static bool GetPrimos(int num)
+            {
+                if (num == 0 || num == 1)
+                    return false;
+
+                for (int i = num; i > num / 2; i++)
+                    return num % i == 0 ? false : true;
+
+                return true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Predicates();
+        }
+    }
+}
---
+++ PredicatesWithClasses/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicatesWithClasses
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public static bool Exists(Person person)
+        {
+            return person.Name.Equals(User.Input);
+        }
+
+        public static bool GetByAge(Person person)
+        {
+            var isNumber = Int32.TryParse(User.Input, out int age);
+
+            if (isNumber)
+                return person.Age.Equals(age);
+            else
+                return false;
+        }
+    }
+
+    class User
+    {
+        public static string Input { get; set; }
+    }
+
+    class Program
+    {
+        static void Searcher(string input)
+        {
+            User.Input = input;
+
+            Predicate<Person> predicateByName = new Predicate<Person>(Person.Exists);
+            Predicate<Person> predicateByAge = new Predicate<Person>(Person.GetByAge);
+
+            List<Person> people = new List<Person>()
+            {
+                new Person() {Name = "Ricardo", Age = 23},
+                new Person() {Name = "Jair", Age = 24},
+                new Person() {Name = "Orozco", Age = 25},
+                new Person() {Name = "Alvarez", Age = 26}
+            };
+
+            if(people.Exists(predicateByName))
+                Console.WriteLine("User Exists!");
+            else
+            {
+                var result = people.FindAll(predicateByAge);
+
+                if (result.Any())
+                {
+                    foreach (var item in result)
+                        Console.WriteLine(item.Name);
+                }
+
+                else
+                    Console.WriteLine("Names don't find");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Search: ");
+            var input = Console.ReadLine();
+
+            Searcher(input);
+        }
+    }
+}
