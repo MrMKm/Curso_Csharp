6c4120f62725b6d395cde977556875c707a65b52
Ricardo Orozco <mrmkm@live.com>
2022-04-12 13:07:28 -0500

    LINQ exercises Day 2

---
+++ LINQExercise_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExercise_2
+{
+    class Filters
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
